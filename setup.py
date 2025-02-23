7149d262143f4cc1fb1f3318bf1576323833638f
lilydjwg <lilydjwg@gmail.com>
2025-02-23 11:15:33 +0800

    use pyproject.toml to build

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# vim: set fileencoding=utf-8:
-import sys
 from setuptools import setup, Extension
 
 def ext_modules(build=False):
@@ -10,42 +8,14 @@ def ext_modules(build=False):
   ext = [Extension('fontconfig', sources, libraries=["fontconfig"])]
   return ext
 
-args = dict(
-  name='Python-fontconfig',
-  version='0.6.0',
-  author='Vayn a.k.a. VT',
-  author_email='vayn@vayn.de',
-  url='https://github.com/Vayn/python-fontconfig',
-  license='LICENSE.txt',
-  description='Python bindings for Fontconfig library',
-  long_description=open('README.rst').read(),
-
-  classifiers = [
-      'Development Status :: 4 - Beta',
-      'Intended Audience :: Developers',
-      'Intended Audience :: System Administrators',
-      'License :: OSI Approved :: GNU General Public License (GPL)',
-      'Operating System :: POSIX',
-      'Operating System :: MacOS :: MacOS X',
-      'Topic :: Text Processing :: Fonts',
-      'Topic :: Utilities',
-      'Programming Language :: Python :: 2',
-      'Programming Language :: Python :: 2.7',
-      'Programming Language :: Python :: 3',
-      'Programming Language :: Python :: 3.2',
-      'Programming Language :: Python :: 3.3',
-      'Programming Language :: Cython',
-  ]
-)
-
+args = {}
 
 if __name__ == '__main__':
-  if sys.argv[1] == 'build_ext':
+  try:
     from Cython.Distutils import build_ext
-
     args.update(cmdclass={'build_ext': build_ext})
+  except ImportError:
     args.update(ext_modules=ext_modules(True))
-    setup(**args)
   else:
     args.update(ext_modules=ext_modules())
-    setup(**args)
+  setup(**args)
