0e40a8b11e88d50f6c956c2e6f140b0bac4d9748
lilydjwg <lilydjwg@gmail.com>
2025-07-12 16:53:53 +0800

    update some setup config

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     from Cython.Distutils import build_ext
     args.update(cmdclass={'build_ext': build_ext})
   except ImportError:
-    args.update(ext_modules=ext_modules(True))
+    args.update(ext_modules=ext_modules(False))
   else:
-    args.update(ext_modules=ext_modules())
+    args.update(ext_modules=ext_modules(True))
   setup(**args)
